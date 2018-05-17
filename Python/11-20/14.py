b0738470839da40ad14940916d2e18b5270ef592
Cooper Stimson <cooperstimson@gmail.com>
2018-05-16 23:16:59 -0700

    O(n) solution to 14.

--- Python/11-20/14.py
+++ Python/11-20/14.py
@@ -1,51 +1,61 @@
-import math
+import datetime
+
+TAB = '\t'
 
 
-def collatz(n, d):
-    cur = n
-    count = 0
-    while cur > 1:
-        if cur in d.keys():
-            count += d[cur]
-            break
-        elif (cur % 2) == 0:
-            cur *= 0.5
-        elif (cur % 2) == 1:
-            cur = 1 + (3 * cur)
-        count += 1
-    return count
+class Collatz(dict):
+
+    def __init__(self, *args, **kwargs):
+        self.maxkey = 0
+        super(Collatz, self).__init__(*args, **kwargs)
+        self.update({0: 0, 1: 1})
+
+    def __setitem__(self, key, item):
+        if item > self.maxval:
+            self.maxkey = key
+        super(Collatz, self).__setitem__(key, item)
+
+    def __getitem__(self, i):
+        if i not in self:
+            self[i] = self[self.collatz(i)] + 1
+        return super(Collatz, self).__getitem__(i)
+
+    @staticmethod
+    def collatz(i):
+        return (i // 2) if (i % 2 == 0) else (3 * i + 1)
+
+    @property
+    def maxval(self):
+        return self[self.maxkey]
+
+    @classmethod
+    def main(cls, exp, verbose=False):
+        now = datetime.datetime.utcnow()
+        collatz = cls()
+        upper_limit = 10 ** exp
+        for i in range(1, upper_limit):
+            if i not in collatz:
+                _ = collatz[i]
+                if verbose:
+                    print(
+                        len(collatz),
+                        int(1000 * len(collatz) / i) / 1000,
+                        TAB,
+                        i,
+                        TAB,
+                        collatz[i],
+                        TAB,
+                        collatz.collatz(i),
+                        collatz[collatz.collatz(i)],
+                    )
+        print(TAB, collatz.maxkey, collatz.maxval)
+        duration = datetime.datetime.utcnow() - now
+        print(TAB, duration, duration / upper_limit)
 
 
-def isprime(n):
-    primes = [2, 3]
-    for i in range(5, int(math.ceil(math.sqrt(n)))):
-        composite = False
-        for p in primes:
-            if i % p == 0:
-                composite = True
-                break
-        if not composite:
-            primes.append(i)
-    for p in primes:
-        if n % p == 0:
-            return False
-    return True
-
-
-def main():
-    d = {}
-    m = 0
-    for i in range(500000, 1000000):
-        if isprime(i):
-            if isprime(i % 500):
-                print("...", str(100.0 * float(i) / float(1000000)) + "%")
-            current = collatz(i, d)
-            if current > m:
-                m = current
-                print(m)
-                d[i] = current
-    print(m)
-
-
-if __name__ == '__main':
-    main()
+if __name__ == '__main__':
+    for limit in range(1, 7):
+        if limit:
+            print('-' * 7)
+        print('N =', limit)
+        Collatz.main(limit)
